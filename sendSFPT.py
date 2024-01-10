d7462577e6325d3b98ba7c4a7dd7d82a97127856
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-01-10 20:05:08 -0300

    reverted to no threads, too much errors :c

--- sendSFPT.py
+++ sendSFPT.py
@@ -2,8 +2,7 @@ import os
 from dotenv import load_dotenv
 import paramiko
 import argparse
-import concurrent.futures  # Add this import
-from tqdm import tqdm
+import subprocess
 
 
 def load_credentials():
@@ -16,58 +15,39 @@ def load_credentials():
     return username
 
 
-def sftp_upload_file(
-    local_file_path, remote_folder, hostname, port, username, password
-):
-    try:
-        # Create an SSH client
-        transport = paramiko.Transport((hostname, port))
-        transport.connect(username=username, password=password)
+def sftp_upload_folder(local_folder, remote_folder, hostname, port, username, password):
+    # Create an SSH client
+    transport = paramiko.Transport((hostname, port))
+    transport.connect(username=username, password=password)
 
-        # Create an SFTP client
-        sftp = paramiko.SFTPClient.from_transport(transport)
+    # Create an SFTP client
+    sftp = paramiko.SFTPClient.from_transport(transport)
 
-        # Get the file name from the local path
-        local_file_name = os.path.basename(local_file_path)
-
-        # Print the name of the file before uploading
-        print("Uploading:", local_file_name)
-
-        # Upload the file
+    # Iterate through all files in the local folder
+    for local_file_name in os.listdir(local_folder):
+        local_file_path = os.path.join(local_folder, local_file_name)
         remote_file_path = os.path.join(remote_folder, local_file_name)
+
+        # Upload each file
         sftp.put(local_file_path, remote_file_path)
         print("Uploaded:", local_file_name)
 
-    except Exception as e:
-        print(f"Error uploading {local_file_path}: {str(e)}")
-
-    finally:
-        # Close the connections
-        sftp.close()
-        transport.close()
+    # Close the connections
+    sftp.close()
+    transport.close()
 
 
-def sftp_upload_folder(
-    local_folder, remote_folder, hostname, port, username, password, max_threads=12
-):
-    # Create an executor with a ThreadPool to upload files concurrently
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
-        # Submit the upload tasks for each file in the local folder
-        futures = [
-            executor.submit(
-                sftp_upload_file,
-                os.path.join(local_folder, local_file_name),
-                remote_folder,
-                hostname,
-                port,
-                username,
-                password,
-            )
-            for local_file_name in os.listdir(local_folder)
-        ]
+def run_generate_csv_script(local_folder):
+    # Get the directory of the current script
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Wait for all tasks to complete
-        concurrent.futures.wait(futures)
+    # Construct the full path to the generateCSVwithFileNames.py script
+    generate_csv_script_path = os.path.join(
+        current_script_dir, "generateCSVwithFileNames.py"
+    )
+
+    # Run the generateCSVwithFileNames.py script
+    subprocess.run(["python", generate_csv_script_path, local_folder])
 
 
 if __name__ == "__main__":
@@ -93,7 +73,7 @@ if __name__ == "__main__":
     # Prompt user for password
     password = input("Enter the password for {}: ".format(username))
 
-    # Upload files from local folder to remote folder concurrently without progress bar
+    # Upload files from local folder to remote folder
     sftp_upload_folder(
         args.local_folder, remote_folder_path, hostname, port, username, password
     )
