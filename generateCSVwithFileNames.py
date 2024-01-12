e944d19c5cbdd9ff51f5f9d3a52ddc6d2fb7a01b
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-01-12 01:43:55 -0300

    Refactor to getGPTAnswer and finally fix the doble quote in title, amem

--- generateCSVwithFileNames.py
+++ generateCSVwithFileNames.py
@@ -10,7 +10,6 @@ client = OpenAI()
 
 gptModel = "gpt-3.5-turbo-1106"
 # gptmodel = "gpt-4"
-
 prompts_file_path = "prompts.txt"
 
 def find_prompt_for_filename(filename_base):
@@ -27,6 +26,23 @@ def find_prompt_for_filename(filename_base):
     print("NOT FOUND")
     return None
 
+def getGPTResponse(model, role, content):
+    gptPrompt = [
+        {"role": "system", "content": "You are an award-winning director of photography specialized in stock photography."},
+        {"role": role, "content": content},
+    ]
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=gptPrompt,
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    return response.choices[0].message.content.strip()
 
 def get_category():
     # Prompt the user for the category
@@ -35,72 +51,13 @@ def get_category():
 
 
 def translate_title(title):
-    # Use GPT-4 to translate the title
-
-    gptPrompt = [
-        {
-            "role": "system",
-            "content": "You are an award-winning director of photography specialized in stock photography.",
-        },
-        {
-            "role": "user",
-            "content": f"I'm going to give you a title in English and you should translate it to Portuguese, the title should not be longer than 200 letters! \n\n {title} \n just give me the translation with no other information in your response.",
-        },
-    ]
-
-    response = client.chat.completions.create(
-        model=gptModel,
-        messages=gptPrompt,
-        temperature=1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-
-    # Extract the translated text from the GPT-4 response
-    gptAnswer  = response.choices[0].message.content
-
-    return gptAnswer
+    gptTitle = getGPTResponse(gptModel, "user", f"I'm going to give you a title in English and you should translate it to Portuguese, the title should not be longer than 200 letters! \n\n {title} \n just give me the translation with no other information in your response. Don't use any pontualtion like (',.-!)")
+    return gptTitle
 
 
 def getKeywords(title):
-    gptPrompt = [
-        {
-            "role": "system",
-            "content": "You are an award-winning director of photography specialized in stock photography.",
-        },
-        {
-            "role": "user",
-            "content": f"""
-                Me dê 30 palavras chaves que sejam relacionadas ao seguinte título de uma imagem: 
-                {title}
-                Organize as palavras chave por ordem de relevância, e nunca utilize palavras genéricas como 'imagem', ou 'cena'. 
-                Todas as palavras chave devem ser separadas por vírgulas 
-                Just give me the keywords in portuguese with no other information in your response.
-                Here is an example:
-                Title: Uma imagem visualmente marcante de um soprador de vidro criando arte abstrata de vidro, mostrando a fluidez e transparência do meio na criação de formas únicas e intrincadas.
-                Keywords: soprador de vidro, arte abstrata, fluidez, transparência, vidro, criação, formas únicas, intrincadas, artesanato, ofício, material, habilidade, artista, obra de arte, artístico, criatividade, colorido, vidro soprado, técnica, processo, produção, fundição, moldagem, soprando, soprador, escultura, moldar, soprar, derretimento, soprando vidro, vidro fundido
-                ",
-            """,
-        },
-    ]
-
-    response = client.chat.completions.create(
-        model=gptModel,
-        messages=gptPrompt,
-        temperature=1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-
-    # Extract the translated text from the GPT-4 response
-    gptAnswer  = response.choices[0].message.content
-
-    return gptAnswer 
-
+    gptKeywords = getGPTResponse(gptModel, "user", f"Me dê 30 palavras chaves que sejam relacionadas ao seguinte título de uma imagem: \n{title}\nOrganize as palavras chave por ordem de relevância, e nunca utilize palavras genéricas como 'imagem', ou 'cena'.\nTodas as palavras chave devem ser separadas por vírgulas\nJust give me the keywords in portuguese with no other information in your response.\nHere is an example:")
+    return gptKeywords
 
 def create_csv(folder_path):
     parent_folder_name = os.path.basename(
@@ -143,9 +100,9 @@ def create_csv(folder_path):
                 # Prompt for title, keywords, and category for each unique filename
                 
                 gptTitle = translate_title(fullPrompt)
+                gptTitle = gptTitle.replace(',', '').replace('.', '')
 
                 gptKeywords = getKeywords(gptTitle)
-
                 category = category.strip()
 
                 # Remove leading and trailing whitespaces
@@ -170,7 +127,7 @@ def create_csv(folder_path):
             writer.writerow(
                 {
                     "Filename": file,
-                    "Title": filename_info[filename_base]["Title"],
+                    "Title": filename_info[filename_base]["Title"].strip('",.-'),
                     "Keywords": filename_info[filename_base]["Keywords"],
                     "Category": filename_info[filename_base]["Category"],
                     "Releases": "",
@@ -197,10 +154,3 @@ if __name__ == "__main__":
 
     # Call the function to create the CSV file
     create_csv(args.folder_path)
-
-    parent_folder_name = os.path.basename(
-        os.path.normpath(os.path.join(args.folder_path, ".."))
-    )
-    csv_file_name = f"{parent_folder_name}_output.csv"
-    # Remove pesky quotes
-    subprocess.run(['python3', 'wtfAreThisQuotes.py', csv_file_name])
