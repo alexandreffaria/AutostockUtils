d8d641291312ea972af240aea34b9109689488fe
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-02-02 00:35:11 -0300

    start of the refactor, the behaviour on break is wrong

--- pinocchio.py
+++ pinocchio.py
@@ -5,43 +5,56 @@ import random
 from datetime import datetime, timedelta
 
 if len(sys.argv) != 2:
-    print("Usage: python script.py <file_path>")
+    print("Usage: python3 pinocchio.py <prompt list.txt>")
     sys.exit(1)
 
-start_time = datetime.strptime("08:00", "%H:%M") + timedelta(minutes=random.uniform(0,15))
-end_time = datetime.strptime("21:00", "%H:%M") + timedelta(minutes=random.uniform(0,15))
+def getWorkHours():
+    print("Getting working hours....")
+    start_time = datetime.strptime("08:00", "%H:%M") + timedelta(minutes=random.uniform(0,15))
+    end_time = datetime.strptime("21:00", "%H:%M") + timedelta(minutes=random.uniform(0,15))
+    print(f"Working hours are: {start_time}-{end_time}")
+    return start_time, end_time
 
-file_path = sys.argv[1]
+def sendPrompt(prompt):
+    pyau.moveTo(550,720)
+
+    pyau.click()
+    time.sleep(5)
+
+    pyau.typewrite("/imaine")
+    time.sleep(10)
+
+    pyau.press("enter")
+    time.sleep(5)
+
+    pyau.typewrite(prompt.strip())
+    time.sleep(5)
+    pyau.typewrite(". Sony FE 24-70mm f/2.8 GM cinematic, shot by hasselblad X1D, editorial photogtaphy ")
+    pyau.typewrite("--ar 16:9 --style raw")
+
+    pyau.press("enter")
+
+
+
+promptsList = sys.argv[1] # Prompt list
 
 time.sleep(10)
+
+
+start_time, end_time = getWorkHours()
 
 while True:
         
     current_time = datetime.now().time()
+    print(f"Agora: {current_time}")
 
     if start_time.time() <= current_time <= end_time.time():
 
-        with open(file_path, 'r') as file:
+        with open(promptsList, 'r') as f:
 
-            for line in file:
+            for prompt in f:
 
-                pyau.moveTo(550,720)
-
-                pyau.click()
-                time.sleep(1)
-
-                pyau.typewrite("/imagine")
-                time.sleep(2)
-
-                pyau.press("enter")
-                time.sleep(1)
-
-                pyau.typewrite(line.strip())
-                time.sleep(1)
-                pyau.typewrite(". Sony FE 24-70mm f/2.8 GM cinematic, shot by hasselblad X1D, editorial photogtaphy ")
-                pyau.typewrite("--ar 16:9 --style raw")
-
-                pyau.press("enter")
+                sendPrompt(prompt)
 
                 sleep_duration = random.uniform(3 * 60, 7 * 60)
 
