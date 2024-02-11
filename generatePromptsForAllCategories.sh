2bb6629bf9156508e8f200b948e7fee3f6fc53e6
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-02-11 00:41:05 -0300

    organizing

--- generatePromptsForAllCategories.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Loop through categories from 1 to 21
-for category in {1..21}; do
-    # Run the script with the specified parameters
-    python3 promptGenerator.py --category $category --strategy "FOLLOW MY RULES" --amount 250
-done
