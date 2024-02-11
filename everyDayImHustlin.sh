c380587d4f85da70f07b2570b61862580dce3886
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-02-11 01:34:40 -0300

    i thought i done it?

--- everyDayImHustlin.sh
+++ everyDayImHustlin.sh
@@ -20,4 +20,4 @@ echo "Starting pinocchio with: ${files[index]}"
 index=$((index % ${#files[@]}))
 echo "$index" > dailyIndexFile
 # # Execute pinocchio.py with the next file
-python3 "/home/meulindux/Desktop/AutostockUtils/pinocchio.py" "${files[index]}"
+python3 "/home/meulindux/Desktop/AutostockUtils/Utils/pinocchio.py" "${files[index]}"
