ec91493488d67db68f90534c3b3ceffa22fe6c42
Alexandre Faria <alexandreffaria@me.com>
2024-02-11 22:24:42 -0300

    removing preload

--- Utils/qc.py
+++ Utils/qc.py
@@ -19,15 +19,11 @@ class ImageViewer(QGraphicsView):
         self.folder_path = folder_path
         self.files = files
         self.current_index = 0
-        self.preload_count = 50  # Number of images to preload
 
         self.scene = QGraphicsScene(self)
         self.setScene(self.scene)
 
-        self.preloaded_images = {}  # Dictionary to store preloaded images
-
         self.load_image()
-        self.preload_images()
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -66,12 +62,10 @@ class ImageViewer(QGraphicsView):
             self.current_index = len(self.files) - 1
 
         self.load_image()
-        self.preload_images()
 
     def next_image(self):
         self.current_index = (self.current_index + 1) % len(self.files)
         self.load_image()
-        self.preload_images()
 
     def previous_image(self):
         self.current_index = (self.current_index - 1) % len(self.files)
@@ -90,7 +84,6 @@ class ImageViewer(QGraphicsView):
             self.current_index = len(self.files) - 1
 
         self.load_image()
-        self.preload_images()
 
     def load_image(self):
         if not self.files:
@@ -99,16 +92,9 @@ class ImageViewer(QGraphicsView):
         file_name = self.files[self.current_index]
         file_path = os.path.join(self.folder_path, file_name)
 
-        # Check if the image is already preloaded
-        if file_name in self.preloaded_images:
-            img = self.preloaded_images[file_name]
-        else:
-            # Load image using imageio with FreeImage (GPU acceleration)
-            with imageio.get_reader(file_path) as reader:
-                img = reader.get_data(0)  # Read the first frame
-
-            # Save the image to the preload cache
-            self.preloaded_images[file_name] = img
+        # Load image using imageio with FreeImage (GPU acceleration)
+        with imageio.get_reader(file_path) as reader:
+            img = reader.get_data(0)  # Read the first frame
 
         height, width, channels = img.shape
 
@@ -118,9 +104,7 @@ class ImageViewer(QGraphicsView):
         )
 
         # Set window title with current file index
-        self.setWindowTitle(
-            f"{file_name} - {self.current_index + 1}/{len(self.files)}"
-        )
+        self.setWindowTitle(f"{file_name} - {self.current_index + 1}/{len(self.files)}")
         # Get the size of the viewport
         view_size = self.viewport().size()
 
@@ -132,20 +116,6 @@ class ImageViewer(QGraphicsView):
         item = QGraphicsPixmapItem(scaled_image)
         self.scene.clear()
         self.scene.addItem(item)
-
-    def preload_images(self):
-        # Preload the next 50 images into memory
-        for i in range(
-            self.current_index + 1,
-            min(len(self.files), self.current_index + 1 + self.preload_count),
-        ):
-            file_name = self.files[i]
-            file_path = os.path.join(self.folder_path, file_name)
-
-            if file_name not in self.preloaded_images:
-                with imageio.get_reader(file_path) as reader:
-                    img = reader.get_data(0)
-                self.preloaded_images[file_name] = img
 
 
 if __name__ == "__main__":
