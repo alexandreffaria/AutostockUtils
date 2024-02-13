849c1afda7122a5c095eb2bfe279b8a5d33c04e4
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-02-13 15:15:32 -0300

    prompting engeneering and stuff

--- GenerateCSV/gptApi.py
+++ GenerateCSV/gptApi.py
@@ -36,35 +36,28 @@ def getGPTResponse(content):
 def getKeywords(title):
     gptKeywords = getGPTResponse(
 f"""
-Me dê 30 palavras chaves que sejam relacionadas ao título de uma imagem que vou te passar no fim dessa mensagem.
-Organize as palavras chave por ordem de relevância, e nunca utilize palavras genéricas como 'imagem', ou 'cena'.
-Todas as palavras chave devem ser separadas por vírgulas
-Apenas me de as palavras-chave me portugues sem nenhuma outra informação em sua resposta
-
-EXEMPLO:
-ENTRADA:
-Uma jovem moça aproveitando um momento aconchegante perto de uma lareira lendo um livro em atmosfera tranquila
-SAÍDA:
-mulheres, livro, em casa, gente, sorridente, adolescente, quarto, lazer, cama, ler, beldade, sentado, sofá, rir, pessoa, estudante, educação
-
-TÍTULO: {title}
+Me dê 50 palavras chaves que sejam relacionadas a esse título. Separadas por virgulas e ordenadas por ordem de relevância:
+{title}
 """
-    )
-    return gptKeywords.replace('"', "")
+)
+    return gptKeywords.replace('"', "").replace(".", "")
 
 def createTitle(title):
     gptTitle = getGPTResponse(
 f"""
-I'm going to give you a title in English and you should create an similar one but in Portuguese, the title should not be longer than 200 letters! 
-Just give me the translation with no other information in your response. Don't use any pontualtion like (',.-!)
-Here is and example:
+I'm going to give you a description of an image in English and you should create a title summarizing the description, try to use every important point of the description, 
+Here is an example:
+
 INPUT:
 A close up shot with a 50mm lens capturing a young woman enjoying a cozy moment by the fireplace with a book in hand and a warm blanket draped around her shoulders Soft natural light illuminates the scene creating a tranquil and peaceful atmosphere
 OUTPUT:
 Uma jovem moça aproveitando um momento aconchegante perto de uma lareira lendo um livro em atmosfera tranquila com um cobertor quente nos ombros
 
+***the title should have 20 words maximum! ***
+***the title should be in Brazilian Portuguese***
+
 Here is the title:
 {title}
 """,
-    )
+)
     return gptTitle.replace('"', "")
