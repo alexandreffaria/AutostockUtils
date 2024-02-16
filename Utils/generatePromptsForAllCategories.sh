28c611772741be7950942bc5204c060d5f487984
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-02-16 01:48:27 -0300

    fixing path

--- Utils/generatePromptsForAllCategories.sh
+++ Utils/generatePromptsForAllCategories.sh
@@ -3,5 +3,5 @@
 # Loop through categories from 1 to 21
 for category in {1..21}; do
     # Run the script with the specified parameters
-    python3 promptGenerator.py --category $category --strategy "FOLLOW MY RULES" --amount 250
+    python3 Utils/promptGenerator.py --category $category --strategy "FOLLOW MY RULES" --amount 250
 done
