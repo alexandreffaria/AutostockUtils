2115b0a0284b04ff2f2d64dcbe28b0773bcc5b1b
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-02-25 15:35:56 -0300

    removing grabing coordinates to it's own file, and passing the pc in use to setup the right coords

--- Utils/deleteVecteezy.py
+++ Utils/deleteVecteezy.py
@@ -2,32 +2,31 @@ import pyautogui as pyau
 import time
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 pinocchio.py <number of pages to delete>")
+if len(sys.argv) != 3:
+    print("Usage: python3 script.py <number of pages to delete> <medusa or oldboi>")
     sys.exit(1)
 
 
-def deleteWholePage(nPages):
+def deleteWholePage(nPages, pc):
+    if pc == "medusa":
+        selectAll = (1210, 352)
+        deleteIcon = (1794, 292)
+        confirmDelete = (916, 616)
+    if pc == "oldboi":
+        selectAll = (1197, 316)
+        deleteIcon = (1781, 328)
+        confirmDelete = (914, 661)
+
     for _ in range(nPages):
-        pyau.moveTo(1197, 316)
+        pyau.moveTo(selectAll)
         pyau.click()
         time.sleep(1)
-        pyau.moveTo(1781, 328)
+        pyau.moveTo(deleteIcon)
         pyau.click()
-        pyau.moveTo(914, 661)
+        pyau.moveTo(confirmDelete)
         pyau.click()
         time.sleep(1)
         time.sleep(8)
 
 
-deleteWholePage(int(sys.argv[1]))
-
-# while True:
-
-#     # Get the current mouse position
-#     x, y = pyau.position()
-
-#     # Print the coordinates
-#     print("Mouse X coordinate:", x)
-#     print("Mouse Y coordinate:", y)
-#     time.sleep(3)
+deleteWholePage(int(sys.argv[1]), sys.argv[2])
