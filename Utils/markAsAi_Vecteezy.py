0aa64138ab8001fffc72e694bba56b484f281d81
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-02-27 02:07:50 -0300

    mark as ai for vecteezy

---
+++ Utils/markAsAi_Vecteezy.py
@@ -0,0 +1,39 @@
+import pyautogui as pyau
+import time
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python3 markAsAi_Vecteezy.py <number of pages to mark as AI> <name of pc>")
+    sys.exit(1)
+
+
+def deleteWholePage(nPages, pc):
+    if pc == "medusa":
+        pass
+    if pc == "oldboi":
+        selectAll = (1188,321)
+        AiGenerated = (1571,779)
+        selectSoftware = (1572,825)
+        midJourney = (1557,898)
+        submitFor = (1588,1021)
+        submit = (647,714)
+        accept = (890,799)
+        closeWindow = (958,711)
+
+    clicks = [selectAll,AiGenerated,selectSoftware,midJourney,submitFor,submit, accept,closeWindow]
+
+    for _ in range(nPages):
+        for i in range(7):
+            pyau.moveTo(clicks[i])
+            pyau.click()
+            time.sleep(2)
+            
+        pyau.press('f5')
+        time.sleep(5)
+
+
+pages = int(sys.argv[1])
+print(pages)
+pc = sys.argv[2]
+print(pc)
+deleteWholePage(pages, pc)
