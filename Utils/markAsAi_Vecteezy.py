33a1b7fff4bbad7ec2ce61abab9e37aedb7f2151
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-02-27 02:45:52 -0300

    timing

--- Utils/markAsAi_Vecteezy.py
+++ Utils/markAsAi_Vecteezy.py
@@ -1,6 +1,7 @@
 import pyautogui as pyau
 import time
 import sys
+from tqdm import tqdm
 
 if len(sys.argv) != 3:
     print("Usage: python3 markAsAi_Vecteezy.py <number of pages to mark as AI> <name of pc>")
@@ -11,29 +12,28 @@ def deleteWholePage(nPages, pc):
     if pc == "medusa":
         pass
     if pc == "oldboi":
-        selectAll = (1188,321)
-        AiGenerated = (1571,779)
-        selectSoftware = (1572,825)
-        midJourney = (1557,898)
-        submitFor = (1588,1021)
-        submit = (647,714)
-        accept = (890,799)
-        closeWindow = (958,711)
+        selectAll = (1188, 321)
+        AiGenerated = (1571, 779)
+        selectSoftware = (1572, 825)
+        midJourney = (1557, 898)
+        submitFor = (1588, 1021)
+        submit = (647, 714)
+        accept = (890, 799)
 
-    clicks = [selectAll,AiGenerated,selectSoftware,midJourney,submitFor,submit, accept,closeWindow]
+    clicks = [selectAll, AiGenerated, selectSoftware, midJourney, submitFor, submit, accept]
 
-    for _ in range(nPages):
+    for _ in tqdm(range(nPages), desc="Processing Pages", unit="page"):
         for i in range(7):
             pyau.moveTo(clicks[i])
             pyau.click()
-            time.sleep(2)
-            
+            time.sleep(1)
+        
+        time.sleep(2)
         pyau.press('f5')
-        time.sleep(5)
+        time.sleep(4)
 
 
 pages = int(sys.argv[1])
-print(pages)
 pc = sys.argv[2]
-print(pc)
+
 deleteWholePage(pages, pc)
