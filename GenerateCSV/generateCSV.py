51e6b3dca33d4e5e4e485e7c7dfc76bf0606edbd
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-03-23 02:11:51 -0300

    mistakes were made?

--- GenerateCSV/generateCSV.py
+++ GenerateCSV/generateCSV.py
@@ -44,7 +44,6 @@ def create_csv(folder_path, output_folder, category, prompts_file_path, platform
     # Get a list of all files in the specified folder
     files = [
         f for f in os.listdir(folder_path)
-        f for f in os.listdir(folder_path)
         if os.path.isfile(os.path.join(folder_path, f))
     ]
 
@@ -163,54 +162,6 @@ def create_csv(folder_path, output_folder, category, prompts_file_path, platform
 
     print(f"CSV file created successfully: {os.path.abspath(csv_file_path)}")
     print(f"Processing complete. {len(set(files))} unique files processed.")
-
-
-def main():
-    root = tk.Tk()
-    root.title("Select Category and Folder")
-    def on_close():
-        root.quit()
-        root.destroy()
-        sys.exit()
-
-    def process():
-        selected_category = category_var.get()
-        category_key = selected_category.split("-")[0]  # Extracting the category key (number)
-        category = int(category_key)
-        folder_path = folder_var.get()
-        prompts_file_name = get_prompts_file_name(category)
-        prompts_file_path = os.path.join(prompts_folder_path, prompts_file_name)
-        create_csv(folder_path, category, prompts_file_path)
-
-    try:
-        # Category Selection
-        category_label = tk.Label(root, text="Select a Category:")
-        category_label.grid(row=0, column=0, padx=10, pady=5)
-        category_var = tk.StringVar(root)
-        category_options = [f"{key}-{value}" for key, value in categorias.items()]
-        category_dropdown = tk.OptionMenu(root, category_var, *category_options)
-        category_dropdown.grid(row=0, column=1, padx=10, pady=5)
-
-        # Folder Selection
-        folder_label = tk.Label(root, text="Select Folder:")
-        folder_label.grid(row=1, column=0, padx=10, pady=5)
-        folder_var = tk.StringVar(root)
-        folder_entry = tk.Entry(root, textvariable=folder_var, state="disabled")
-        folder_entry.grid(row=1, column=1, padx=10, pady=5)
-        folder_button = tk.Button(
-            root, text="Browse", command=lambda: folder_var.set(getFolder())
-        )
-        folder_button.grid(row=1, column=2, padx=10, pady=5)
-
-        # Process Button
-        process_button = tk.Button(root, text="Process", command=process)
-        process_button.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
-
-        root.protocol("WM_DELETE_WINDOW", on_close)
-        root.mainloop()
-
-    except KeyboardInterrupt:
-        on_close()  # Explicitly destroy the Tkinter root window
 
 
 def main():
