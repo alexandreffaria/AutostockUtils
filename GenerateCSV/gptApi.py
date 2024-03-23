f830b80343a46ac05a6735bcd93504b13d695612
Alexandre F <alexandrefonsecaffaria@gmail.com>
2024-03-23 00:47:00 -0300

    making the prompt more clear, hopefully

--- GenerateCSV/gptApi.py
+++ GenerateCSV/gptApi.py
@@ -40,9 +40,9 @@ def getKeywords(title, language):
         gptKeywords = getGPTResponse(
             f"""
 Me dê 40 palavras-chave que sejam relacionadas a esse título. Separadas por virgulas e ordenadas por ordem de relevância. Aqui está um exemplo:
-Título:
+INPUT:
 Faces Diversas: Emoções e Determinação em uma Comunidade Inclusiva
-Palavras-chave:
+OUTPUT:
 emoções, determinação, comunidade inclusiva, diversidade, inclusão, aceitação, igualdade, respeito, empoderamento, diferenças, interação social, coletividade, pertencimento, autoestima, autoaceitação, tolerância, solidariedade, expressão emocional, apoio mútuo, convivência harmoniosa, inclusão social, diálogo aberto, compreensão, diversidade cultural, empatia, apoio emocional, autoconfiança, superação, desenvolvimento pessoal, integração, reconhecimento, celebração, pluralidade, bem-estar emocional, autoconhecimento, cidadania, identidade, amor próprio, comunicação eficaz, colaboração, união.
 
 Título:
