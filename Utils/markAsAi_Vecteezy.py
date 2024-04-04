563c5e80bc7d000359f0458df137318efe2f9869
Alexandre Faria <alexandreffaria@me.com>
2024-04-04 13:56:40 -0300

    adding some quality of life and updating coordinates

--- Utils/markAsAi_Vecteezy.py
+++ Utils/markAsAi_Vecteezy.py
@@ -1,6 +1,7 @@
 import pyautogui as pyau
 import time
 import sys
+import keyboard
 from tqdm import tqdm
 
 if len(sys.argv) != 3:
@@ -10,14 +11,14 @@ if len(sys.argv) != 3:
 
 def deleteWholePage(nPages, pc):
     if pc == "medusa":
-        selectAll = (1233,1366)
-        AiGenerated = (1593,1822)
-        selectSoftware = (1592,1868)
-        midJourney = (1563,1951)
-        submitFor = (1583,2101)
-        submit = (656,1777)
-        accept =  (909,1874)
-    if pc == "oldboi":
+        selectAll = (1233, 296)
+        AiGenerated = (1593, 745)
+        selectSoftware = (1592, 791)
+        midJourney = (1563, 875)
+        submitFor = (1583, 985)
+        submit = (656, 672)
+        accept = (909, 763)
+    elif pc == "oldboi":
         selectAll = (1188, 321)
         AiGenerated = (1571, 779)
         selectSoftware = (1572, 825)
@@ -25,6 +26,9 @@ def deleteWholePage(nPages, pc):
         submitFor = (1588, 1021)
         submit = (647, 714)
         accept = (890, 799)
+    else:
+        print("Invalid PC name.")
+        sys.exit(1)
 
     clicks = [selectAll, AiGenerated, selectSoftware, midJourney, submitFor, submit, accept]
 
@@ -37,6 +41,19 @@ def deleteWholePage(nPages, pc):
         time.sleep(2)
         pyau.press('f5')
         time.sleep(4)
+
+        if keyboard.is_pressed('esc'):
+            print("Exiting...")
+            sys.exit(0)
+        elif keyboard.is_pressed('p'):
+            print("Paused. Press 'p' to continue or 'esc' to kill.")
+            while True:
+                if keyboard.is_pressed('p'):
+                    print("Resuming...")
+                    break
+                elif keyboard.is_pressed('esc'):
+                    print("Exiting...")
+                    sys.exit(0)
 
 
 pages = int(sys.argv[1])
