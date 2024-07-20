f46f6169d17ce026ba63c17cecae5262f97b6a2b
Alexandre Faria <alexandrefonsecaffaria@gmail.com>
2024-07-20 15:53:53 -0300

    prompts from the new model

--- Utils/generatePromptsForAllCategories.sh
+++ Utils/generatePromptsForAllCategories.sh
@@ -3,5 +3,5 @@
 # Loop through categories from 1 to 21
 for category in {1..21}; do
     # Run the script with the specified parameters
-    python3 Utils/promptGenerator.py --category $category --strategy "15 at a time" --amount 500
+    python3 Utils/promptGenerator.py --category $category --strategy "4o-mini model" --amount 500
 done
