bf1552c96e2b0b9df40e4139cabea080848a4556
Alexandre Faria <alexandreffaria@me.com>
2024-07-24 01:56:41 -0300

    saving settings baby

--- autostock.py
+++ autostock.py
@@ -5,9 +5,22 @@ from tkinter.font import Font
 from subprocess import Popen, PIPE
 from GenerateCSV.categorias import categorias
 import random
+import json
+
+SETTINGS_FILE = "settings.json"
 
 accent_color_list = ["#800f00", "#004080", "#80005e", "#800000"]
 accent_color = random.choice(accent_color_list)
+
+def load_settings():
+    if os.path.exists(SETTINGS_FILE):
+        with open(SETTINGS_FILE, "r") as file:
+            return json.load(file)
+    return {}
+
+def save_settings(settings):
+    with open(SETTINGS_FILE, "w") as file:
+        json.dump(settings, file, indent=4)
 
 # Function to run a shell command Function to run a shell command
 def run_command(command):
@@ -74,6 +87,28 @@ def upload(folder_path, platform):
         run_command(command)
     
 def process_workflow():
+
+    selected_adobe = adobe_var.get() 
+    selected_freepik = freepik_var.get()
+    selected_upscale = upscale_var.get()
+    selected_convertToJPG = convertToJPG_var.get()
+    selected_createCSV = createCSV_var.get()
+    selected_upload = upload_var.get()
+    selected_NoPrompt = noPrompt_var.get()
+    selected_language = language_var.get()
+
+    settings = {
+        "selected_adobe": selected_adobe,
+        "selected_freepik": selected_freepik,
+        "selected_upscale": selected_upscale,
+        "selected_convertToJPG": selected_convertToJPG,
+        "selected_createCSV": selected_createCSV,
+        "selected_upload": selected_upload,
+        "selected_NoPrompt": selected_NoPrompt,
+        "selected_language": selected_language,
+    } 
+    save_settings(settings)
+
     folder_path = folder_var.get()
     if not folder_path:
         show_custom_error("A gente precisa de uma pasta bebê.")
@@ -84,15 +119,6 @@ def process_workflow():
         show_custom_error("A gente precisa de uma categoria bebê.")
         return
     
-    selected_adobe = adobe_var.get() 
-    selected_freepik = freepik_var.get()
-    selected_upscale = upscale_var.get()
-    selected_convertToJPG = convertToJPG_var.get()
-    selected_createCSV = createCSV_var.get()
-    selected_upload = upload_var.get()
-    selected_NoPrompt = noPrompt_var.get()
-    selected_language = language_var.get()
-
     if selected_adobe:
         if selected_upscale:
             upscale(folder_path)
@@ -138,9 +164,10 @@ root.wm_iconbitmap('meulindo.ico')
 bold_font = Font(family="Arial", size=12, weight="bold")
 small_font = Font(family="Arial", size=9)
 
+settings = load_settings()
 
 # Folder selection
-folder_var = tk.StringVar()
+folder_var = tk.StringVar(value=settings.get("folder_path", ""))
 folder_label = tk.Label(root,text="Deixa eu fazer procê ❤️", bg="#2b2b2b", fg="#ffffff", font=bold_font, height=1)
 folder_label.pack(pady=5, padx=20)
 
@@ -150,8 +177,7 @@ folder_button = tk.Button(root, text="🔎", command=select_folder, bg=accent_co
 folder_button.pack(pady=20)
 
 # Category selection
-category_var = tk.StringVar()
-category_var.set("Categoria")
+category_var = tk.StringVar(value=settings.get("selected_category", "Categoria"))
 
 category_menu = tk.Menu(root, bg="#4d4d4d", fg="#ffffff")  # Configure the background and foreground colors for the dropdown menu
 root.config(menu=category_menu)
@@ -170,33 +196,30 @@ qc_button.pack(pady=5)
 process_labels = tk.Label(root,text="Quais processos?", bg="#2b2b2b", fg="#ffffff", font=bold_font, height=1)
 process_labels.pack(pady=5, padx=20)
 
-upscale_var = tk.BooleanVar(value=True)  
+upscale_var = tk.BooleanVar(value=settings.get("selected_upscale", True))
 upscale_checkbox = tk.Checkbutton(root, text="Upscale", variable=upscale_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 upscale_checkbox.pack(pady=5)
 
-convertToJPG_var = tk.BooleanVar(value=True)  
+convertToJPG_var = tk.BooleanVar(value=settings.get("selected_convertToJPG", True))
 convertToJPG_checkbox = tk.Checkbutton(root, text="Convert to JPG", variable=convertToJPG_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 convertToJPG_checkbox.pack(pady=5)
 
-createCSV_var = tk.BooleanVar(value=True)  
+createCSV_var = tk.BooleanVar(value=settings.get("selected_createCSV", True))
 createCSV_checkbox = tk.Checkbutton(root, text="Generate CSV's", variable=createCSV_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 createCSV_checkbox.pack(pady=5)
 
-upload_var = tk.BooleanVar(value=False)  
+upload_var = tk.BooleanVar(value=settings.get("selected_upload", False))
 upload_checkbox = tk.Checkbutton(root, text="Upload Files", variable=upload_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 upload_checkbox.pack(pady=5)
 
-noPrompt_var = tk.BooleanVar(value=False)  
+noPrompt_var = tk.BooleanVar(value=settings.get("selected_NoPrompt", False))
 noPrompt_checkbox = tk.Checkbutton(root, text="Generate CSV's without prompt", variable=noPrompt_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 noPrompt_checkbox.pack(pady=5)
-
 
 platform_labels = tk.Label(root,text="Qual idioma pra adobe?", bg="#2b2b2b", fg="#ffffff", font=small_font, height=1)
 platform_labels.pack(pady=5, padx=20)
 
-language_var = tk.StringVar()
-language_var.set("en")
-
+language_var = tk.StringVar(value=settings.get("selected_language", "en"))
 language_menu = tk.OptionMenu(root, language_var, "pt", "en")
 language_menu.config(bg="#4d4d4d", fg="#ffffff")  # Adjust background and foreground colors
 language_menu.pack(pady=5)
@@ -205,11 +228,11 @@ language_menu.config(highlightthickness=1, highlightbackground=accent_color)
 platform_labels = tk.Label(root,text="Quais plataformas?", bg="#2b2b2b", fg="#ffffff", font=bold_font, height=1)
 platform_labels.pack(pady=5, padx=20)
 
-adobe_var = tk.BooleanVar(value=True)  
+adobe_var = tk.BooleanVar(value=settings.get("selected_adobe", True))
 adobe_checkbox = tk.Checkbutton(root, text="Adobe", variable=adobe_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff" )
 adobe_checkbox.pack(pady=5)
 
-freepik_var = tk.BooleanVar(value=True)  
+freepik_var = tk.BooleanVar(value=settings.get("selected_freepik", True))
 freepik_checkbox = tk.Checkbutton(root, text="Freepik", variable=freepik_var, bg="#2b2b2b", fg="#ffffff", selectcolor=accent_color,activebackground="#2b2b2b", activeforeground="#fff")
 freepik_checkbox.pack(pady=5)
 
