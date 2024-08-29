c6fa3a284a1b480a3af6dce8be3329282b4655da
Alex. F <alexandrefonsecaffaria@gmail.com>
2024-08-28 22:04:36 -0300

    checking 8am addding it to crontab @reboot

--- everyDayImHustlin.sh
+++ everyDayImHustlin.sh
@@ -6,18 +6,30 @@ source /home/meulindux/AutostockUtils/venv/bin/activate
 export DISPLAY=:0
 exec > >(tee -a log) 2>&1
 
-# Loop through each file in the folder, sorted numerically
-while IFS= read -r -d '' file; do
-    # Check if the file is a regular file (not a directory)
-    if [ -f "$file" ]; then
-        files+=("$file") # Add the file to the array
-    fi
-done < <(find "$folder" -maxdepth 1 -type f -name '*.txt' -print0 | sort -zV)
+# Get the current hour
+current_hour=$(date +%H)
 
-index=$(<dailyIndexFile)
-echo "Starting pinocchio with: ${files[index]}"
-((index++))
-index=$((index % ${#files[@]}))
-echo "$index" > dailyIndexFile
-# # Execute pinocchio.py with the next file
-python3 "/home/meulindux/AutostockUtils/Utils/multiPinocchio.py" "${files[index]}"
+# Check if the current time is after 8 AM
+if [ "$current_hour" -ge 8 ]; then
+    # Initialize the files array
+    files=()
+
+    # Loop through each file in the folder, sorted numerically
+    while IFS= read -r -d '' file; do
+        # Check if the file is a regular file (not a directory)
+        if [ -f "$file" ]; then
+            files+=("$file") # Add the file to the array
+        fi
+    done < <(find "$folder" -maxdepth 1 -type f -name '*.txt' -print0 | sort -zV)
+
+    index=$(<dailyIndexFile)
+    echo "Starting pinocchio with: ${files[index]}"
+    ((index++))
+    index=$((index % ${#files[@]}))
+    echo "$index" > dailyIndexFile
+
+    # Execute pinocchio.py with the next file
+    python3 "/home/meulindux/AutostockUtils/Utils/multiPinocchio.py" "${files[index]}"
+else
+    echo "Not running because it's before 8 AM."
+fi
