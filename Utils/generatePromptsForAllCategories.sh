e7cd139ac7d69bd761603fbd769d7445e6b213cd
Alex F. <alexandrefonsecaffaria@gmail.com>
2024-12-27 17:44:26 -0300

    fresh start

--- Utils/generatePromptsForAllCategories.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Loop through categories from 1 to 21
-for category in {1..21}; do
-    # Run the script with the specified parameters
-    python3 Utils/promptGenerator.py --category $category --strategy "4o-mini model" --amount 500
-done
